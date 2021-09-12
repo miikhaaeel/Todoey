7adaf38a889b29590148701a9a4abfe98ac6e74a
Miikhaaeel <fadowomichael1@gmail.com>
2021-09-12 16:27:16 +0100

    UI state

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -2,22 +2,47 @@ import 'package:flutter/material.dart';
 
 import '../constants.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   final String? taskTitle;
 
   TaskTile({this.taskTitle});
 
   @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        taskTitle as String,
+        widget.taskTitle as String,
         style: kChecklistTextStyle,
       ),
-      trailing: Checkbox(
-        onChanged: (value) => true,
-        value: false,
+      trailing: TaskCheckBox(
+        checkBoxState: isChecked,
       ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool? checkBoxState;
+
+  TaskCheckBox({this.checkBoxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.green,
+      value: checkBoxState,
+      onChanged: (value) {
+        // setState(() {
+        //   isChecked = value!;
+        // });
+      },
     );
   }
 }
